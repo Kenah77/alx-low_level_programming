963b12c68bed3f5f90408454a38f6afef3fb3420
saam kenah <saamkenah@gmail.com>
2022-03-23 09:40:38 -0700

    SAAM ATUM KEZIE KENAH and Mofa Minette Njomi forever in love amen

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,26 +1,21 @@
-#include "holberton.h"
-
+#include "main.h"
+#include <stdio.h>
 /**
-  * print_number - print numbers chars
-  * @n: integer params
-  * Return: 0
- **/
-
-void print_number(int n)
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	unsigned int n1;
-
-	n1 = n;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		n1 = -n;
-	}
-
-	if (n1 / 10 != 0)
-	{
-		print_number(n1 / 10);
-	}
-	_putchar((n1 % 10) + '0');
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-98);
+    _putchar('\n');
+    return (0);
 }
