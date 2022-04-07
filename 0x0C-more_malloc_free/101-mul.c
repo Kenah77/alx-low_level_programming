4a0de94c6196021ab9cbad094a365a21b125485b
saam kenah <saamkenah@gmail.com>
2022-04-07 08:29:37 -0700

    My personal work

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2, mu1;
+    
+    printf("Enter num1:");
+    printf("Enter an integer: ");
+    scanf("%d",&mu1);
+
+    for(num1=1,; i<=10; num2=num1++)
+    {
+        printf("\n%d * %d = %d", num2, num1, num2*num1);
+    }
+    
+    return 98;
+}
